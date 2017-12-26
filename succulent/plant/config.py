89854d2b504f34658ee0459dc5de486a14875aab
RiverAndNoon <2809695868@qq.com>
2017-12-26 18:39:45 +0800

    Initial commit

---
+++ succulent/plant/config.py
@@ -0,0 +1,40 @@
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+#通用配置
+class Config:
+    #密钥
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '12345'
+    # 数据库
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 邮件发送
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.163.com'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'm2809695868@163.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'shao08251400'
+
+    UPLOADED_PHOTOS_DEST = os.path.join(base_dir,'static/icon')
+    MAX_CONTENT_LENGTH = 2*1024*1024
+    #额外的初始化操作，即使什么内容都没有写，也是有意义的
+    @staticmethod
+    def init_app(app):
+        pass
+
+#开发环境
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir,'blog_dev.sqlite')
+#测试环境
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir,'blog_test.sqlite')
+#生成环境
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir,'blog.sqlite')
+
+#配置字典
+config = {
+    'development' : DevelopmentConfig,
+    'testing' : TestingConfig,
+    'production' : ProductionConfig,
+    'default' : DevelopmentConfig
+}
---
+++ succulent/plant/extensions.py
@@ -0,0 +1,43 @@
+#导入类库
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_login import LoginManager
+from flask_uploads import UploadSet,IMAGES,configure_uploads,patch_request_class
+
+#创建对象
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+moment = Moment()
+migrate = Migrate(db=db)
+login_manager = LoginManager()
+photos = UploadSet('photos',IMAGES)
+
+#初始化
+def config_extensions(app):
+    bootstrap.init_app(app)
+    db.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    migrate.init_app(app)
+    #登录认证
+    login_manager.init_app(app)
+    #指定登录的端点
+    login_manager.login_view = 'user.login'
+    #需要登录才能访问的提示信息
+    login_manager.login_message = '需要登录才能访问'
+
+    #设置session保护级别
+    #None：禁用session保护
+    # 'basic'：基本的保护，默认选项
+    # 'strong'：最严格的保护，一旦用户登录信息改变，立即退出登录
+    login_manager.session_protection = 'strong'
+
+    configure_uploads(app,photos)
+    patch_request_class(app,size=None)
+
+
+
