de5af5ce1a05b47588604c1b23061457569097cc
97612336 <xkjskk@163.com>
2018-01-08 18:31:59 +0800

    创建图片表

---
+++ succulent/plant/models/images.py
@@ -0,0 +1,10 @@
+from plant.extensions import db
+
+
+class Image(db.Model):
+    __tablename__ = 'image'
+    id = db.Column(db.Integer, primary_key=True)
+    url=db.Column(db.String(300))
+
+    #关联文章一对多外键
+    posts_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
