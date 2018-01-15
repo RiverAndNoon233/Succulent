b1c0eb9871c8bd2bfcd447096f882ea1c4eaa5f6
97612336 <xkjskk@163.com>
2018-01-15 18:05:10 +0800

    提交首页展示文章API

---
+++ succulent/plant/views/duoduo/__init__.py
@@ -0,0 +1,3 @@
+from plant.views.duoduo.get_vistitesd_num import Get_visited_nums_API
+
+Get_visited_nums_API()
---
+++ succulent/plant/views/duoduo/get_vistitesd_num.py
@@ -0,0 +1,59 @@
+
+from flask import  request
+
+from plant.extensions import api, Resource, db, moment
+from plant.models import User, Posts, Image
+
+class Get_visited_nums_API(Resource):
+    def post(self):
+        # 得到文章的页码数
+        page = request.json.get('page')
+        # 分页查询文章,按照浏览量排序
+        posts = Posts.query.order_by(db.desc(Posts.count))
+
+        #判断文章输入的页数是否在正确的范围内，不在则返回404
+        try:
+            page_posts=posts.paginate(page=page,per_page=10)
+        except:
+            return {'code': 404, 'msg': '没有数据', 'data': None}
+
+        # 创建data列表，用来存储返回每个文章组成的字典
+        data = []
+
+        # 分析分页对象，得到分页对象中的具体数据，并把数据返回给调用者
+        posts = page_posts.items
+        for one_post in posts:
+            # 得到对象的每一个值，然后以字典的形式存储，添加到data字典中
+            one_data = {}
+            # 得到这篇文章的用户对象
+            user = one_post.user
+            # 得到用户头像
+            one_data['user_icon'] = user.image
+            # 得到用户名
+            one_data['user_name'] = user.nickname
+            # 得到文章的id
+            one_data['essay_id'] = one_post.id
+            # 得到文章标题
+            one_data['essay_title'] = one_post.title
+            # 得到文章的图片
+            images = one_post.image
+            # 遍历图片得到前三张图片
+            image_list = []
+            i = 1
+            for one_img in images:
+                one_img_url = one_img.url
+                image_list.append(one_img_url)
+                i = i + 1
+                if i > 3:
+                    break
+            one_data['essay_image'] = image_list
+            # 得到文章时间
+            one_data['essay_time'] = one_post.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+            # 得到文章的浏览量
+            one_data['essay_views_num'] = one_post.count
+
+            data.append(one_data)
+
+        return {'code': 200, 'msg': '获取成功', 'data': data}
+
+api.add_resource(Get_visited_nums_API, '/api/v1/index/hotartical')
